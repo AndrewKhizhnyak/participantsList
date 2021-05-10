19c0ac164bd9c375521e86cacfd71d6dd6ad0cb1
Andrew Khizhnyak <andrew_khizhnyak@mail.ru>
2021-05-10 23:24:14 +0300

    This fixes deploy script

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-
-git push -f git@github.com:andrewkhizhnyak/participantsList.git main:gh-pages
+git push -f git@github.com:andrewkhizhnyak/participantsList.git master:gh-pages
 
 cd -
